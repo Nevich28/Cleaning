e200037808ea40ad547f62db0ea9d5c4e79abb79
Andrey <andrey.liubashchenko@gmail.com>
2023-02-19 19:04:26 +0200

    slider+2 blosk

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,9 +1,27 @@
 window.addEventListener('DOMContentLoaded', () => {
+	//Управление гамбургером
 	const hamburger = document.querySelector('.hamburger'),
-		menu = document.querySelector('.menu__items');
+		menu = document.querySelector('.menu__items'),
+		menuItem = document.querySelectorAll('.menu__item');
 
 	hamburger.addEventListener('touchstart', () => {
 		hamburger.classList.toggle('hamburger__cross');
 		menu.classList.toggle('menu_activ');
+	});
+
+	menuItem.forEach(item => {
+		item.addEventListener('click', () => {
+			hamburger.classList.toggle('hamburger__cross');
+			menu.classList.toggle('menu_activ');
+		});
+	});
+
+	//Slick slider
+	$('.main_slider__items').slick({
+		autoplay: false,
+		dots: true,
+		speed: 1000,
+		//autoplaySpeed: 6000,
+		//setting-name: setting-value
 	});
 });
