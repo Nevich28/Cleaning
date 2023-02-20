fd217e6a247af15897bb8fc4ddf42bbc0d925337
Andrey <andrey.liubashchenko@gmail.com>
2023-02-21 00:11:09 +0200

    to advantages

--- src/js/script.js
+++ src/js/script.js
@@ -16,11 +16,63 @@ window.addEventListener('DOMContentLoaded', () => {
 		});
 	});
 
-	//Slick slider
+	//Slick slider1
 	$('.main_slider__items').slick({
-		autoplay: false,
+		autoplay: true,
 		dots: true,
 		speed: 1000,
+		arrows: false,
+		//autoplaySpeed: 6000,
+		//setting-name: setting-value
+	});
+
+	//Slick slider1
+	$('.services__slider').slick({
+		infinite: true,
+		slidesToShow: 3,
+		centerMode: true,
+		responsive: [
+			{
+				breakpoint: 1300,
+				settings: {
+					arrows: false,
+					// centerMode: true,
+					// centerPadding: '40px',
+					// slidesToShow: 3
+				}
+			},{
+				breakpoint: 992,
+				settings: {
+					// 		arrows: false,
+					centerMode: false,
+					slidesToShow: 2,
+					arrows: false,
+					autoplay: true,
+					speed: 1000,
+					// 		centerPadding: '40px',
+					// 		slidesToShow: 1
+				}
+			},{
+				breakpoint: 576,
+				settings: {
+					// 		arrows: false,
+					centerMode: false,
+					slidesToShow: 1,
+					arrows: false,
+					autoplay: true,
+					speed: 1000,
+					// 		centerPadding: '40px',
+					// 		slidesToShow: 1
+				}
+			}
+		],
+		//slidesToScroll: 3,
+		// centerMode: true,
+		// centerPadding: '60px',
+		// autoplay: true,
+		// dots: true,
+		// speed: 1000,
+		// arrows: false,
 		//autoplaySpeed: 6000,
 		//setting-name: setting-value
 	});
